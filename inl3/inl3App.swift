dea46180ee86b5be554b26a9a63900d3804b45b5
Andreas Ekdahl <fixed-term.andreas.ekdahl@se.bosch.com>
2023-10-11 18:01:34 +0200

    Initial Commit

---
+++ inl3/inl3App.swift
@@ -0,0 +1,17 @@
+//
+//  inl3App.swift
+//  inl3
+//
+//  Created by Andreas Ekdahl on 2023-10-11.
+//
+
+import SwiftUI
+
+@main
+struct inl3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
